360f0f5ad3e08fbe4bc60dffca908e1f799535b8
Super Genius <ken+git@gnus.ai>
2024-04-17 17:05:41 -0700

    Start of Circuits for Proof system

---
+++ src/circuits/TwoFactor.cpp
@@ -0,0 +1,10 @@
+#include <nil/crypto3/algebra/curves/pallas.hpp>
+
+using namespace nil::crypto3::algebra::curves;
+
+[[circuit]] typename pallas::template g1_type<nil::crypto3::algebra::curves::coordinates::affine>::value_type curve_operations(
+        typename pallas::template g1_type<nil::crypto3::algebra::curves::coordinates::affine>::value_type a,
+        typename pallas::template g1_type<nil::crypto3::algebra::curves::coordinates::affine>::value_type b) {
+
+    return a + b;
+}
