0e2f749844ea2fbfb3dd17c198481995736a31d1
Super Genius <ken+git@gnus.ai>
2024-09-25 18:27:44 -0700

    Almost working MPC testing circuit, need to figure out why calculations are incorrect for this

--- include/MPCVerifierCircuit.hpp
+++ include/MPCVerifierCircuit.hpp
@@ -1,4 +1,11 @@
-#include "MPCVerifierCircuit.hpp"
+//
+// Created by Your Name on 10/04/24.
+//
+
+#ifndef MPCVERIFIERCIRCUIT_HPP
+#define MPCVERIFIERCIRCUIT_HPP
+
+#include <nil/crypto3/algebra/curves/pallas.hpp>
 
 using namespace nil::crypto3::algebra::curves;
 
@@ -12,19 +19,6 @@ using namespace nil::crypto3::algebra::curves;
     typename pallas::template g1_type<nil::crypto3::algebra::curves::coordinates::affine>::value_type expected_new_balance_commitment,
     const typename pallas::template g1_type<nil::crypto3::algebra::curves::coordinates::affine>::value_type& generator,
     typename pallas::template g1_type<nil::crypto3::algebra::curves::coordinates::affine>::value_type total_aggregate // Sum of the final values from all nodes
-) {
-    // Check if the total aggregate equals the expected total
-    bool aggregate_validity = (total_aggregate == total_random_sum); // Expecting the total_random_sum to be precomputed with * 2
+);
 
-    // Validate balance conditions
-    bool balance_conditions = (balance >= amount);
-
-    // Validate commitments
-    bool valid_balance_commitment = (balance_commitment == (generator * balance));
-    bool valid_amount_commitment = (amount_commitment == (generator * amount));
-    bool valid_new_balance_commitment = (expected_new_balance_commitment == (generator * (balance - amount)));
-
-    // Ensure all conditions are met
-    return aggregate_validity && balance_conditions &&
-           valid_balance_commitment && valid_amount_commitment && valid_new_balance_commitment;
-}
+#endif //MPCVERIFIERCIRCUIT_HPP
